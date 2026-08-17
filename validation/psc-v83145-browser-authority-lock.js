add1a1c9c2f3303fb896ebe28c4be16ae9dcaef6
liemduongtarot <liemduongtarot@gmail.com>
2026-08-17 06:53:47 +0100

    Add V8.3.145 immutable fixture and browser lock

---
+++ validation/psc-v83145-browser-authority-lock.js
@@ -0,0 +1,7 @@
+(function(global){
+'use strict';
+if(!global.QCSemanticCoreV14)throw new Error('V8.3.145 browser authority lock requires QCSemanticCoreV14');
+global.QCSemanticCoreV4=global.QCSemanticCoreV14;
+global.PSC_V83145_BROWSER=Object.freeze({version:'V8.3.145-BROWSER-AUTHORITY-LOCK',semantic_authority:'QCSemanticCoreV14',expected_semantic_version:'V8.3.145-ROUTE-FIRST-EVIDENCE-STATE-REPAIR-CANDIDATE',production_authorized:false,step_111:'prohibited'});
+if(global.document&&global.document.documentElement)global.document.documentElement.dataset.pscBrowserAuthority='V8.3.145:QCSemanticCoreV14';
+})(typeof globalThis!=='undefined'?globalThis:this);
