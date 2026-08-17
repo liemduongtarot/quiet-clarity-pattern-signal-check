8c720f89e4ce8140c285dffe885e6f4f8735069e
liemduongtarot <liemduongtarot@gmail.com>
2026-08-17 05:13:54 +0100

    Add V8.3.144 browser authority lock

---
+++ validation/psc-v83144-browser-authority-lock.js
@@ -0,0 +1,7 @@
+(function(global){
+'use strict';
+if(!global.QCSemanticCoreV13)throw new Error('V8.3.144 browser authority lock requires QCSemanticCoreV13');
+global.QCSemanticCoreV4=global.QCSemanticCoreV13;
+global.PSC_V83144_BROWSER=Object.freeze({version:'V8.3.144-BROWSER-AUTHORITY-LOCK',semantic_authority:'QCSemanticCoreV13',expected_semantic_version:'V8.3.144-SEALED-A-FAILURE-CONVERGENCE',production_authorized:false,step_111:'prohibited'});
+if(global.document&&global.document.documentElement)global.document.documentElement.dataset.pscBrowserAuthority='V8.3.144:QCSemanticCoreV13';
+})(typeof globalThis!=='undefined'?globalThis:this);
